8d18072ec26ba6ea830b54d6e2fa73715c5e9fce
Nikita <nikitankionline@gmail.com>
2023-06-01 16:23:36 +0300

    updating 2

---
+++ Assets/Scripts/SnapScrolling.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapScrolling : MonoBehaviour
+{
+    public int panCount;
+
+    public GameObject panPrefab;
+}
